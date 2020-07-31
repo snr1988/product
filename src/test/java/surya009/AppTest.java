6edc157c3a8061234d8a8ecb293a51159cca5ecd
suryach <csnarayana1988@email.com>
2020-07-31 12:17:50 +0530

    1st

---
+++ src/test/java/surya009/AppTest.java
@@ -0,0 +1,20 @@
+package surya009;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
